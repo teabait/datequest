9dcd183f937998cfc979c315b165bbcd47505546
teabait <hello.teaho@gmail.com>
2013-12-18 15:09:34 -0500

    took out broken line in env

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 DateQuest::Application.initialize!
-system "RAILS_ENV=development bin/delayed_job start"
+
