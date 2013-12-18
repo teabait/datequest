758fa0ccde9b2323694901d129995ee95c39fa2e
teabait <hello.teaho@gmail.com>
2013-12-18 17:16:55 -0500

    test coverage check. nothing is broken.

--- Gemfile
+++ Gemfile
@@ -52,6 +52,7 @@ end
 
 group :test do
   gem 'shoulda-matchers'
+  gem 'simplecov', require: false
 end
 
 group :development, :test do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,10 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-
+require 'simplecov'
 require 'capybara/rspec'
 require 'capybara/rails'
+SimpleCov.start
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
